0635737b7086805dfa626988c3c3956c48150002
Rujvi <nagpurerujvi1201@gmail.com>
2021-06-10 23:38:53 +0530

    props_trending

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -22,7 +22,16 @@ setContent(data.results);
         <div className="trending">{
             content &&
             content.map((c) => (
-              <SingleContent />
+              <SingleContent 
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type={c.media_type}
+              vote_average={c.vote_average}
+
+              />
             ))
         }
 

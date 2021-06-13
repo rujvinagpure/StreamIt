28e5981114bc807b3ea82f8055bb0f02130eec41
Rujvi <nagpurerujvi1201@gmail.com>
2021-06-13 18:06:35 +0530

    search half

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,9 +1,54 @@
-const trending = () => {
+import { useState } from "react";
+import {
+    Button,
+    createMuiTheme,
+    Tab,
+    Tabs,
+    TextField,
+    ThemeProvider,
+  } from "@material-ui/core"; 
+  import SearchIcon from "@material-ui/icons/Search";
+const Search = () => {
+ 
+
+    const [type, setType] = useState(0);
+
+    const darkTheme = createMuiTheme({
+        palette: {
+          type: "dark",
+          primary: {
+            main: "#fff",
+          },
+        },
+      });
+
+
     return (
-        <div>
-            <span className="pageTitle">Search</span>
+        <div className="search">
+            <ThemeProvider theme ={darkTheme}>
+<div style={{display:"flex", margin:"15px 0"}}>
+
+<TextField
+            style={{ flex: 1 }}
+            className="searchBox"
+            label="Search"
+            variant="filled"
+            //onChange={(e) => setSearchText(e.target.value)}
+          />
+<Button variant="contained" style={{marginLeft:10}}> <SearchIcon/> </Button>
+
+</div>
+<Tabs value={type} indicatorColor="primary" textColor="primary">
+
+ <Tab style={{ width: "50%" }} label="Search Movies" />
+          <Tab style={{ width: "50%" }} label="Search TV Series" />
+
+</Tabs>
+            
+            </ThemeProvider>
+           
         </div>
     )
 }
 
-export default trending
+export default Search;
